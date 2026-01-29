190e2b9acc8cb89a4bc38bfe6d4474cc4932f47c
Andre passos <andrepassosdossantos64@gmail.com>
2026-01-29 10:46:18 -0300

    Add files via upload
    Aprendendo os conceitos básicos de entrada e saída de dados em Java.

---
+++ oriemtaçãoaritimetica.java
@@ -0,0 +1,16 @@
+package aula_2;
+
+public class oriemtaçãoaritimetica {
+    static void main (){
+        double num = 2 * 6 / 3;
+        double num2 = 3 + 2 * 4;
+        double num3 = (3+2)*4;
+        double num4 = 60 / (3 +2) * 4;
+        double num5 = 60/((3+2)*4);
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+    }
+}
