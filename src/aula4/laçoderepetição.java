c476371e5fde6d67165a15b2c44efa6767ea6ed0
andre2353 <andrepassosdossantos64@gmail.com>
2026-02-05 05:34:28 -0300

    aula 4

---
+++ src/aula4/laçoderepetição.java
@@ -0,0 +1,13 @@
+package aula4;
+
+public class laçoderepetição {
+    static void main() {
+        // ! inicia a variavel
+        // 2 condição
+        // 3 alteração da variavel
+        for (int i = 0; i < 10; i++) {
+            IO.println("o valor de i é : "+ i++);
+        }
+
+    }
+}
--- src/aula_2/EntradaDeDados.java
+++ src/aula_2/EntradaDeDados.java
@@ -1,22 +1,28 @@
-package aula_2;
-
-import java.util.Scanner;
-
-public class EntradaDeDados {
-    static void main () {
-        Scanner sc = new Scanner(System.in);
-        IO.println("digite sua idade");
-        int idade = sc.nextInt();
-        IO.println("digite o seu nome ");
-        String nome = sc.next();
-        IO.println("digite seu peso");
-        double peso = sc.nextDouble();
-        IO.println("digite sua altura");
-        double altura = sc.nextDouble();
-        System.out.println("sua idade " + idade);
-        System.out.println("seu nome " + nome);
-        System.out.println("seu peso " + peso);
-        System.out.println("seu altura " + altura);
-        System.out.println("seu imc " + (peso / (altura * altura)));
-    }
-}
+//package aula_2;
+//
+//import java.util.Scanner;
+//
+//public class EntradaDeDados {
+//    static void main () {
+//        Scanner sc = new Scanner(System.in);
+//        IO.println("digite sua idade");
+//        int idade = sc.nextInt();
+//        IO.println("digite o seu nome ");
+//        String nome = sc.next();
+//        IO.println("digite seu peso");
+//        double peso = sc.nextDouble();
+//        IO.println("digite sua altura");
+//        double altura = sc.nextDouble();
+//        System.out.println("sua idade " + idade);
+//        System.out.println("seu nome " + nome);
+//        System.out.println("seu peso " + peso);
+//        System.out.println("seu altura " + altura);
+//        double imc = peso*(altura*altura)
+//        IO.println(imc);
+//
+//        if (imc <18,5){
+//            IO.println("abaixo do peso" +
+//                    "");
+//        }
+//    }
+//}
